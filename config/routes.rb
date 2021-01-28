07f7d27f66b2646adf73350384338c77ea00ff16
okbaik2 <okbaik2@gmail.com>
2021-01-29 05:52:13 +0900

    comment fail 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-
+  get 'comments/show'
+  get 'posts/show' 
+  resources :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_172135) do
+ActiveRecord::Schema.define(version: 2021_01_28_202337) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
